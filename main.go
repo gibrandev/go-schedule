ae08df3529a22ffecf6c939d8c55515ea1ab6b3e
Gibran Dimasagung <gibrandev@gmail.com>
2022-06-28 13:45:28 +0700

    Update

--- main.go
+++ main.go
@@ -16,15 +16,13 @@ func main() {
 		response, error := http.PostForm("https://eofze3br6mxmqb5.m.pipedream.net", url.Values{"key": {"Value"}, "id": {"123"}})
 		if error != nil {
 			fmt.Println("HTTP call failed:", error)
-			return
-		}
-
-		defer response.Body.Close()
-
-		if response.StatusCode != http.StatusOK {
-			fmt.Println("Non-OK HTTP status:", response.StatusCode)
 		} else {
-			fmt.Println(response.Body)
+			defer response.Body.Close()
+			if response.StatusCode != http.StatusOK {
+				fmt.Println("Non-OK HTTP status:", response.StatusCode)
+			} else {
+				fmt.Println(response.Body)
+			}
 		}
 	})
 
