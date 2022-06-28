97526379bb25911165234bee9aa02912633a085b
Gibran Dimasagung <gibrandev@gmail.com>
2022-06-28 13:04:59 +0700

    First uploads

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-co-op/gocron"
+)
+
+func main() {
+	s := gocron.NewScheduler(time.UTC)
+
+	s.Every(5).Seconds().Do(func() {
+		fmt.Println("Hello")
+	})
+
+	s.StartBlocking()
+}
